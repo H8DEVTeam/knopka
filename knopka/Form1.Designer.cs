fc0d938156456ce50183ccb0cc1d23cded722cfc
out455 <out455@ya.ru>
2016-03-31 23:49:11 +0300

    11111

--- knopka/Form1.Designer.cs
+++ knopka/Form1.Designer.cs
@@ -105,6 +105,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(295, 22);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1";
             // 
             // label1
             // 
